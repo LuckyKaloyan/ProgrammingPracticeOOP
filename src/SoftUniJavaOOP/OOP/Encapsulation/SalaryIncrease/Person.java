12fbf2098394d15062183be8a17278c74a0c319f
Калоян Димитров <luckykaloyan@gmail.com>
2024-02-28 10:35:32 +0200

    Update

--- src/SoftUniJavaOOP/OOP/Encapsulation/SalaryIncrease/Person.java
+++ src/SoftUniJavaOOP/OOP/Encapsulation/SalaryIncrease/Person.java
@@ -1,14 +1,19 @@
-package SoftUniJavaOOP.OOP.Encapsulation.SortByNameAndAge;
+package SoftUniJavaOOP.OOP.Encapsulation.SalaryIncrease;
 
 public class Person {
     private String firstname;
     private String lastname;
     private int age;
+    private double salary;
 
-    public Person(String firstname, String lastname, int age){
+
+    public Person(String firstname, String lastname, int age, double salary){
+
         this.firstname = firstname;
         this.lastname = lastname;
         this.age = age;
+        this.salary = salary;
+
     }
 
     public String getFirstname() {
@@ -37,6 +42,14 @@ public class Person {
 
     @Override
     public String toString() {
-        return String.format("%s %s is %d years old.",firstname,lastname,age);
+        return String.format("%s %s is %.2f leva",firstname,lastname,salary);
     }
+    public void increaseSalary(double percentage) {
+        if (this.getAge() >= 30) {
+            salary = salary * (1 + (percentage / 100));
+        } else {
+            salary = salary * (1 + (percentage / 200));
+        }
+    }
+
 }
