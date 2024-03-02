9a0d902d6ee0425c1409d7c56cae2c9258da43f1
Калоян Димитров <luckykaloyan@gmail.com>
2024-03-02 08:29:34 +0200

    Update

--- src/SoftUniJavaOOP/OOP/WorkingWithAbstraction/Excercise/CardRank/Main.java
+++ src/SoftUniJavaOOP/OOP/WorkingWithAbstraction/Excercise/CardRank/Main.java
@@ -1,7 +1,8 @@
 package SoftUniJavaOOP.OOP.WorkingWithAbstraction.Excercise.CardRank;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Card Ranks:");
         for(CardRank rank: CardRank.values()){
             System.out.println("Ordinal value: "+rank.getOrdinalnumber()+"; Name value: "+rank.name());
