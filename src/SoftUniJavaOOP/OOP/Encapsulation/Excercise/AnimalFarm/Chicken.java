694fec13297b5a2df131d1d82aa5904f9f861ec3
Калоян Димитров <luckykaloyan@gmail.com>
2024-03-08 11:12:01 +0200

    Update

---
+++ src/SoftUniJavaOOP/OOP/Encapsulation/Excercise/AnimalFarm/Chicken.java
@@ -0,0 +1,26 @@
+package SoftUniJavaOOP.OOP.Encapsulation.Excercise.AnimalFarm;
+
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+    private void setName(String name){
+        this.name = name;
+    }
+
+    public double productPerDay(){
+        double a;
+        
+        return a;
+    }
+
+
+}
