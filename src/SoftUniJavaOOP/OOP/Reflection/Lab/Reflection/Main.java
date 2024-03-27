13e2f7fd2b7bcc053a935dda47b0639686fa8ef5
Калоян Димитров <luckykaloyan@gmail.com>
2024-03-27 09:31:47 +0200

    Update

---
+++ src/SoftUniJavaOOP/OOP/Reflection/Lab/Reflection/Main.java
@@ -0,0 +1,20 @@
+package SoftUniJavaOOP.OOP.Reflection.Lab.Reflection;
+
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        System.out.println("class "+ Reflection.class.getSimpleName());
+        System.out.println(Reflection.class.getSuperclass());
+        Class[] interfaces = Reflection.class.getInterfaces();
+
+        for (int i = 0; i < interfaces.length; i++) {
+            System.out.println(interfaces[i]);
+        }
+
+        Reflection reflection = (Reflection) Class.forName(Reflection.class.getName()).getConstructor().newInstance();
+
+        System.out.println(reflection);
+    }
+}
